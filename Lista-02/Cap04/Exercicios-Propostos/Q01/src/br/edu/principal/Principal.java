80d6f954dc3ce728510e2b3f149a01fde4d644cc
Alisson <sousa.alisson07@aluno.ifce.edu.br>
2025-09-16 13:04:48 -0300

    Update Principal.java

--- Lista-02/Cap04/Exercicios-Propostos/Q01/src/br/edu/principal/Principal.java
+++ Lista-02/Cap04/Exercicios-Propostos/Q01/src/br/edu/principal/Principal.java
@@ -24,10 +24,11 @@ public class Principal {
 		media = (n1+n2+n3+n4)/4;
 		
 		if (media >= 7) {
-			System.out.println("Aluno aprovado com nota: " + media);
+			System.out.println("Aluno aprovado com média: " + media);
 		} else {
-			System.out.println("Aluno reprovado com nota: " + media);
+			System.out.println("Aluno reprovado com média: " + media);
 		}
 		
 	}
 }
+
