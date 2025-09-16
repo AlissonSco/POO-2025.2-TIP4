f22367c194b1aba5a8116f91fc5474d9ae41e1a6
Alisson <sousa.alisson07@aluno.ifce.edu.br>
2025-09-16 13:31:54 -0300

    Update Principal.java

--- Lista-02/Cap04/Exercicios-Resolvidos/Q08/src/br/edu/principal/Principal.java
+++ Lista-02/Cap04/Exercicios-Resolvidos/Q08/src/br/edu/principal/Principal.java
@@ -6,85 +6,32 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a ,b ,c ,i;
-		System.out.println("Digite um valor para A");
-		a = sc.nextInt();
-		System.out.println("Digite um valor para B");
-		b = sc.nextInt();
-		System.out.println("Digite um valor para C");
-		c = sc.nextInt();
-		System.out.println("Digite um valor para I(1 , 2 ou 3)");
-		i = sc.nextInt();
-		if (i == 1) {
-		    if (a < b && a < c) {
-		        if (b < c) {
-		            System.out.println("A ordem crescente dos números é: " + a + "-" + b + "-" + c);
-		        } else {
-		            System.out.println("A ordem crescente dos números é: " + a + "-" + c + "-" + b);
-		        }
-		    }
+		int n1,n2,soma,op;
+		double raiz;
+		System.out.println("          MENU          ");
+		System.out.println("1 - Somar dois números");
+		System.out.println("2 - Raiz quadrada de um número");
 
-		    if (b < a && b < c) {
-		        if (a < c) {
-		            System.out.println("A ordem crescente dos números é: " + b + "-" + a + "-" + c);
-		        } else {
-		            System.out.println("A ordem crescente dos números é: " + b + "-" + c + "-" + a);
-		        }
-		    }
-
-		    if (c < a && c < b) {
-		        if (a < b) {
-		            System.out.println("A ordem crescente dos números é: " + c + "-" + a + "-" + b);
-		        } else {
-		            System.out.println("A ordem crescente dos números é: " + c + "-" + b + "-" + a);
-		        }
-		    }
-		}
-		if (i == 2) {
-		    if (a > b && a > c) {
-		        if (b > c) {
-		            System.out.println("A ordem decrescente dos números é: " + a + "-" + b + "-" + c);
-		        } else {
-		            System.out.println("A ordem decrescente dos números é: " + a + "-" + c + "-" + b);
-		        }
-		    }
-		    if (b > a && b > c) {
-		        if (a > c) {
-		            System.out.println("A ordem decrescente dos números é: " + b + "-" + a + "-" + c);
-		        } else {
-		            System.out.println("A ordem decrescente dos números é: " + b + "-" + c + "-" + a);
-		        }
-		    }
-		    if (c > a && c > b) {
-		        if (a > b) {
-		            System.out.println("A ordem decrescente dos números é: " + c + "-" + a + "-" + b);
-		        } else {
-		            System.out.println("A ordem decrescente dos números é: " + c + "-" + b + "-" + a);
-		        }
-		    }
-		}
-		if (i == 3) {
-		    if ((a > b && a > c)) {
-		        if (b < c) {
-		            System.out.println("Maior no meio: " + b + "-" + a + "-" + c);
-		        } else {
-		            System.out.println("Maior no meio: " + c + "-" + a + "-" + b);
-		        }
-		    }
-		    if ((b > a && b > c)) {
-		        if (a < c) {
-		            System.out.println("Maior no meio: " + a + "-" + b + "-" + c);
-		        } else {
-		            System.out.println("Maior no meio: " + c + "-" + b + "-" + a);
-		        }
-		    }   
-		    if ((c > a && c > b)) {
-		        if (a < b) {
-		            System.out.println("Maior no meio: " + a + "-" + c + "-" + b);
-		        } else {
-		            System.out.println("Maior no meio: " + b + "-" + c + "-" + a);
-		        }
-		    }
-		}
+		System.out.println("Digite uma opção:");
+		op = sc.nextInt();
+		if (op == 1) {
+            System.out.println("Digite um valor para o primeiro número:");
+            n1 = sc.nextInt();
+            
+            System.out.println("Digite um valor para o segundo número:");
+            n2 = sc.nextInt();
+            
+            soma = n1 + n2;
+            System.out.println("A soma de " + n1 + " e " + n2 + " é " + soma);
+            
+        } else if (op == 2) {
+            System.out.println("Digite um valor:");
+            n1 = sc.nextInt();
+            
+            raiz = Math.sqrt(n1);
+            System.out.println("A raiz quadrada de " + n1 + " é " + raiz);
+        } else {
+            System.out.println("Opção inválida!");
+        }
 	}
 }
